4aa5daa366837915e5fa33621c047be57ecdcb5e
Ianwaswa <ianwaswa2019@gmail.com>
2024-08-08 14:21:11 +0000

    update code

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a class room
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - Maximum number of students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
