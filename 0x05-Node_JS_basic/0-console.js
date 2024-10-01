5fb8bdc8df11ff47abda5af96b038752169d0d37
Ianwaswa <ianwaswa2019@gmail.com>
2024-10-01 10:20:53 +0000

    update code

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on STDOUT
+ * @param {String} msg The message to display.
+ * @author Ian Juma <https://github.com/Ianwaswa>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
